3002b487cfcc50be6ca0416203e4e40e1a3d02a8
Anton Askling <anton.askling@gmail.com>
2024-06-12 09:05:46 +0200

    add visits app

---
+++ src/saas/views.py
@@ -0,0 +1,26 @@
+import pathlib
+from django.shortcuts import render
+from django.http import HttpResponse
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request, *args, **kwargs):
+    PageVisit.objects.create(path=request.path)
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    my_title = "my Page"
+
+    print("!!!", request.path)
+    my_context = {
+        "page_title" : my_title,
+        "page_visit_count": page_qs.count(),
+        "total_visit_count": qs.count(),
+        }
+    html_template  = "home.html"
+    html_=""
+
+    return render(request, html_template, my_context)
+
+
+
