b018339c1e3f16587f74074ab7906c0fda678cd2
Anton Askling <anton.askling@gmail.com>
2024-06-12 17:27:37 +0200

    update settings file

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,8 +30,8 @@ ALLOWED_HOSTS = [
     '.railway.app'
 ]
 if DEBUG:
-    ALLOWED_HOSTS.append('localhost')
-    ALLOWED_HOSTS.append('127.0.0.1')
+    ALLOWED_HOSTS.extend(['localhost', '127.0.0.1'])
+
 
 # Application definition
 
