81c623f697b903bf7ab8a11ba26745927e85b6cd
Anton Askling <anton.askling@gmail.com>
2024-06-15 15:16:28 +0200

    update staticfile-storage to support newer versions of django

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -153,11 +153,16 @@ STATICFILES_DIRS = [
 # output for manage.py collectstatic
 # local CDN
 STATIC_ROOT = BASE_DIR.parent / "local-cdn"
+# whitenoise cacheable files and compression support
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# whitenoise cacheable files and compression support
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
